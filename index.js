3fb87d43c371949f05453dc20573a0283798436e
Tonmoy-Roy <emonismail69@gmail.com>
2022-11-24 00:00:01 +0600

    db connected

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 
 const express = require('express');
 const cors = require('cors');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('second hand bazar server running')
 })
