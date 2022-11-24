26fc0b93d7f95eeb493a66b104997cd682d01fb7
Tonmoy-Roy <emonismail69@gmail.com>
2022-11-24 13:17:08 +0600

    category loaded

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -11,13 +12,40 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://bazar:SLsH0GJSCRuvTqeU@cluster0.ww1mwol.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
 
 
 
 
 
+async function run(){
+try{
+    const AllCategoriesCollection= client.db('bazar').collection('categories');
+    app.get('/allcategories',async(req,res)=>{
+        const query = {}
+        const options= await AllCategoriesCollection.find(query).toArray();
+        console.log(options)
+        res.send(options)
+    })
+    
+}
+finally{
+
+}
+}
+run().catch(console.log)
+
+
+
+
+        
+    
+    
+    
+          
 
 
 
